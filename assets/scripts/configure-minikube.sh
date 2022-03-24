b52bda30b39fc61854899bf018e38978509a6a46
Hanzla Mateen <hanzlamateen@live.com>
2022-03-24 08:04:00 +0500

    Changes to verify node, nvm, npm, git in script

--- assets/scripts/configure-minikube.sh
+++ assets/scripts/configure-minikube.sh
@@ -1,6 +1,75 @@
-set -e
-# set -x
+#!/bin/bash
 
-echo "Hello"
-sleep 10
-echo "Hello2"
+set -e
+
+#=============
+# Verify Node
+#=============
+
+INSTALL_NODE=false
+if node --version >/dev/null; then
+    echo "node is installed"
+
+    NODE_VERSION=$(node --version)
+
+    # Ensure version is greater than 16
+    NODE_MAJOR=${NODE_VERSION:1:2}
+    if [[ $NODE_MAJOR -lt 16 ]]; then
+        echo "node version is outdated $NODE_VERSION"
+        INSTALL_NODE=true
+    fi
+else
+    echo "node is not installed"
+    INSTALL_NODE=true
+fi
+
+if $INSTALL_NODE; then
+    # Ensure NVM is installed
+    if nvm --version >/dev/null; then
+        echo "nvm is installed"
+    else
+        echo "nvm is not installed"
+
+        apt install curl
+        curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+        source ~/.profile
+    fi
+
+    NVM_VERSION=$(nvm --version)
+    echo "nvm version is $NVM_VERSION"
+
+    nvm install node
+    echo "node is installed"
+
+    NODE_VERSION=$(node --version)
+    echo "node version is $NODE_VERSION"
+fi
+
+#=============
+# Verify Npm
+#=============
+
+if npm --version >/dev/null; then
+    echo "npm is installed"
+
+    NPM_VERSION=$(npm --version)
+    echo "npm version is $NPM_VERSION"
+else
+    echo "npm is not installed"
+    exit 1
+fi
+
+#=============
+# Verify Git
+#=============
+
+if git --version >/dev/null; then
+    echo "git is installed"
+else
+    echo "git is not installed"
+
+    sudo apt install git-all
+fi
+
+GIT_VERSION=$(git --version)
+echo "git version is $GIT_VERSION"
