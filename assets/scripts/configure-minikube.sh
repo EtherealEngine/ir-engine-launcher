89eed9cd83a88a4cd27bd1158493b91edefbf249
Hanzla Mateen <hanzlamateen@live.com>
2022-03-28 12:55:05 +0500

    Changes to docker script

--- assets/scripts/configure-minikube.sh
+++ assets/scripts/configure-minikube.sh
@@ -140,9 +140,8 @@ else
     sudo apt-get update -y
     sudo apt-get install -y ca-certificates curl gnupg lsb-release
 
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
     sudo apt-get update -y
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 fi
