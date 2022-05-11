3c269835ae2ad9279562f54afc5a3c29ce0f9b91
Hanzla Mateen <hanzlamateen@live.com>
2022-05-11 07:12:51 +0500

    Changes for storage class

--- src/constants/Storage.ts
+++ src/constants/Storage.ts
@@ -1,10 +1,33 @@
+/**
+ * https://stackoverflow.com/a/8809472/2077741
+ * @returns
+ */
+export const generateUUID = () => {
+  let d = new Date().getTime() //Timestamp
+  let d2 = (typeof performance !== 'undefined' && performance.now && performance.now() * 1000) || 0 //Time in microseconds since page-load or 0 if unsupported
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    let r = Math.random() * 16 //random number between 0 and 16
+    if (d > 0) {
+      //Use timestamp until depleted
+      r = (d + r) % 16 | 0
+      d = Math.floor(d / 16)
+    } else {
+      //Use microseconds since page-load if supported
+      r = (d2 + r) % 16 | 0
+      d2 = Math.floor(d2 / 16)
+    }
+    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16)
+  })
+}
+
 const Storage = {
   COLOR_MODE: 'COLOR_MODE',
   FORCE_DB_REFRESH: 'FORCE_DB_REFRESH',
   AUTH_SECRET_KEY: 'AUTH_SECRET_UUID_OR_SIMILAR',
   XRENGINE_PATH: 'XRENGINE_PATH',
   PATHS_TABLE: 'paths',
-  VARS_TABLE: 'vars'
+  VARS_TABLE: 'vars',
+  PASSWORD_KEY: generateUUID()
 }
 
 export default Storage
