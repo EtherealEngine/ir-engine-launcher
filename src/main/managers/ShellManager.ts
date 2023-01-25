d33c45b855d813cbbc58ad9b4cb2e18fd4c9cd42
Hanzla Mateen <hanzlamateen@live.com>
2023-01-25 09:56:31 +0500

    Updated shell functions to handler handle windows os.

--- src/main/managers/ShellManager.ts
+++ src/main/managers/ShellManager.ts
@@ -1,13 +1,46 @@
 import childProcess, { ExecException } from 'child_process'
+import log from 'electron-log'
+import os from 'os'
 import { lookup, Program } from 'ps-node'
 
+const _getWSLfilePath = async (scriptFile: string) => {
+  scriptFile = scriptFile.replaceAll('\\','\\\\')
+  const wslPathResponse = await exec(`wsl wslpath ${scriptFile}`)
+
+  if (wslPathResponse.error || wslPathResponse.stderr) {
+    log.error(`Error while executing wslpath ${scriptFile}.`, wslPathResponse.error, wslPathResponse.stderr)
+    throw 'Unable to convert path to wsl path'
+  }
+
+  return wslPathResponse.stdout!.toString().trim()
+}
+
 export const execScriptFile = async (scriptFile: string, args: string[]) => {
-  return await exec(`bash "${scriptFile}" ${args.join(' ')}`)
+  const type = os.type()
+
+  let command = ''
+  if (scriptFile.endsWith('.ps1')) {
+    command = `. "${scriptFile}" ${args.join(' ')}`
+  } else if (type === 'Windows_NT') {
+    scriptFile = await _getWSLfilePath(scriptFile)
+    command = `wsl bash "${scriptFile}" ${args.join(' ')}`
+  } else {
+    command = `bash "${scriptFile}" ${args.join(' ')}`
+  }
+
+  return await exec(command)
 }
 
 export const exec = (command: string): Promise<ShellResponse> => {
+  const type = os.type()
+
+  let shell = '/bin/bash'
+  if (type === 'Windows_NT') {
+    shell = 'powershell.exe'
+  }
+
   return new Promise((resolve) => {
-    childProcess.exec(command, { shell: '/bin/bash' }, (error, stdout, stderr) => resolve({ error, stdout, stderr }))
+    childProcess.exec(command, { shell }, (error, stdout, stderr) => resolve({ error, stdout, stderr }))
   })
 }
 
@@ -17,7 +50,19 @@ export const execStreamScriptFile = async (
   onStdout: (data: any) => void,
   onStderr: (data: any) => void
 ) => {
-  return await execStream(`bash "${scriptFile}" ${args.join(' ')}`, onStdout, onStderr)
+  const type = os.type()
+
+  let command = ''
+  if (scriptFile.endsWith('.ps1')) {
+    command = `. "${scriptFile}" ${args.join(' ')}`
+  } else if (type === 'Windows_NT') {
+    scriptFile = await _getWSLfilePath(scriptFile)
+    command = `wsl bash "${scriptFile}" ${args.join(' ')}`
+  } else {
+    command = `bash "${scriptFile}" ${args.join(' ')}`
+  }
+
+  return await execStream(command, onStdout, onStderr)
 }
 
 export const execStream = (
@@ -25,8 +70,15 @@ export const execStream = (
   onStdout: (data: any) => void,
   onStderr: (data: any) => void
 ): Promise<number | null> => {
+  const type = os.type()
+
+  let shell: undefined | string = undefined
+  if (type === 'Windows_NT') {
+    shell = 'powershell.exe'
+  }
+
   return new Promise((resolve) => {
-    const child = childProcess.exec(command)
+    const child = childProcess.exec(command, { shell })
     child.stdout?.on('data', (data) => {
       onStdout(data)
     })
@@ -54,6 +106,18 @@ export const getProcessList = (command: string) => {
       }
     )
   })
+}
+
+export const cleanseString = (inputString: string) => {
+  let finalString = ''
+
+  for (const char of inputString) {
+    if (char.charCodeAt(0) !== 0) {
+      finalString += char
+    }
+  }
+
+  return finalString
 }
 
 type ShellResponse = {
