61f8628ff6285c9ea7e58652d419b984c9ff9653
Hanzla Mateen <hanzlamateen@live.com>
2023-02-01 16:04:40 +0500

    Powershell file with tested scripts being required.

--- assets/scripts/configure-microk8s-windows.ps1
+++ assets/scripts/configure-microk8s-windows.ps1
@@ -1,8 +1,32 @@
 
-Write-Output "There are a total of $($args.count) arguments"
+Write-Host "There are a total of $($args.count) arguments"
 for ( $i = 0; $i -lt $args.count; $i++ ) {
-    Write-Output "Argument  $i is $($args[$i])"
+    Write-Host "Argument  $i is $($args[$i])"
 }
 
-Write-Output "Hello"
-Write-Output "Hello again"
+Write-Host "Hello"
+
+
+# Reference: https://stackoverflow.com/a/52628883/2077741
+# $processes = Get-Process "*Docker Desktop*"
+# if ($processes.Count -gt 0)
+# {
+#     $processes[0].Kill()
+#     $processes[0].WaitForExit()
+# }
+# Start-Process "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+
+
+# $status = wsl bash -c "echo '111Hanzla' | sudo -S /snap/bin/microk8s status"
+
+# Write-Host $status
+    
+# if ($status) {
+#     Write-Host "Installed"
+# } else {
+#     Write-Host "Not Installed"
+# }
+
+# wsl bash -c "echo '111Hanzla' | sudo -S /snap/bin/microk8s status"
+
+# wsl bash -c "echo '111Hanzla' | sudo -S /snap/bin/microk8s start"
