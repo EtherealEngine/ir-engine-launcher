94ffb0b5ffb2c99124b372a07aa5c0bc22c46ff7
Hanzla Mateen <hanzlamateen@live.com>
2023-02-06 14:56:50 +0500

    Fixed node error

--- assets/scripts/configure-microk8s-linux.sh
+++ assets/scripts/configure-microk8s-linux.sh
@@ -52,6 +52,16 @@ set -e
 
 bash ./check-node.sh "$PASSWORD"
 
+#=================
+# Fix Node Profile
+#=================
+
+source ~/.profile
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
 #=============
 # Verify Npm
 #=============
