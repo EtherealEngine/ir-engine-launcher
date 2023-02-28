42d5d5eb13f34e18a3521ab455e575fb200c2ddb
Hanzla Mateen <hanzlamateen@live.com>
2023-02-28 13:22:36 +0500

    Fixed issue of `Set-Content : Stream was not readable` on windows (#46)

--- assets/scripts/check-hostfile.ps1
+++ assets/scripts/check-hostfile.ps1
@@ -1,4 +1,37 @@
 
+#==========
+# Functions
+# Ref: https://stackoverflow.com/a/24992975
+#==========
+
+function Test-FileLock {
+    param (
+        [parameter(Mandatory = $true)][string]$Path
+    )
+  
+    $oFile = New-Object System.IO.FileInfo $Path
+  
+    if ((Test-Path -Path $Path) -eq $false) {
+        return $false
+    }
+  
+    try {
+        $oStream = $oFile.Open([System.IO.FileMode]::Open, [System.IO.FileAccess]::ReadWrite, [System.IO.FileShare]::None)
+  
+        if ($oStream) {
+            $oStream.Close()
+        }
+        return $false
+    }
+    catch {
+        Write-Host "An error occurred:"
+        Write-Host $_
+        # file is locked by a process.
+        return $true
+    }
+}
+
+
 #===========
 # Parameters
 #===========
@@ -18,81 +51,123 @@ Write-Host "Readonly: $IS_READONLY";
 # Update system hostfile
 #=======================
 
-$hostFilePath = "$env:windir\System32\drivers\etc\hosts";
-$wslIp = wsl hostname -I;
-if ($wslIp -like "* *") {
-    $wslIp = $wslIp.split(" ")[0]
-}
+try {
+    $hostFilePath = "$env:windir\System32\drivers\etc\hosts";
+    $wslIp = wsl hostname -I;
+    if ($wslIp -like "* *") {
+        $wslIp = $wslIp.split(" ")[0]
+    }
+    
+    if (Test-Path $hostFilePath) {
+        #Ref: https://stackoverflow.com/a/7976784
+        $hostFileContent = [System.IO.File]::ReadAllText($hostFilePath);
+    
+        if ($IS_READONLY -eq $false) {
+            # Added this to avoid file in user error.
+            $fileLocked = Test-FileLock -Path $hostFilePath
+            while ($fileLocked -eq $true) {
+                Write-Host "hostfile is locked. Waiting for 2 secs.";
+    
+                Start-Sleep -Seconds 2;
+    
+                $fileLocked = Test-FileLock -Path $hostFilePath
+            }
+        }
+    
+        if ($hostFileContent -like "*local.etherealengine.com*") {
+            if ($hostFileContent -like "*$wslIp local.etherealengine.com*") {
+                Write-Host "*.etherealengine.com host entry exists";
+            }
+            else {
+                Write-Host "*.etherealengine.com host entry outdated";
+    
+                $EXITCODE = 1
+                if ($IS_READONLY -eq $false) {
+                    #Ref: https://shellgeek.com/read-file-line-by-line-in-powershell/
+                    #Ref: https://stackoverflow.com/a/21970035
+                    $updatedContent = [System.IO.File]::ReadAllLines($hostFilePath);
+                    $linenumber = $updatedContent | select-string "local.etherealengine.com";
+                    $updatedContent[$linenumber.LineNumber - 1] = "$wslIp local.etherealengine.com api-local.etherealengine.com instanceserver-local.etherealengine.com 00000.instanceserver-local.etherealengine.com 00001.instanceserver-local.etherealengine.com 00002.instanceserver-local.etherealengine.com 00003.instanceserver-local.etherealengine.com";
 
-if (Test-Path $hostFilePath) {
-    # Added round brackets around get-content to fix file is in user issue.
-    #Ref: https://stackoverflow.com/a/10480811
-    $hostFileContent = (Get-Content $hostFilePath -Raw);
+                    # Added this to avoid file in user error.
+                    $fileLocked = Test-FileLock -Path $hostFilePath
+                    while ($fileLocked -eq $true) {
+                        Write-Host "hostfile is locked. Waiting for 2 secs.";
+    
+                        Start-Sleep -Seconds 2;
+    
+                        $fileLocked = Test-FileLock -Path $hostFilePath
+                    }
 
-    if ($hostFileContent -like "*local.etherealengine.com*") {
-        if ($hostFileContent -like "*$wslIp local.etherealengine.com*") {
-            Write-Host "*.etherealengine.com host entry exists";
+                    [System.IO.File]::WriteAllLines($hostFilePath, $updatedContent);
+                }
+            }
         }
         else {
-            Write-Host "*.etherealengine.com host entry outdated";
-
+            Write-Host "*.etherealengine.com host entry needs to be added";
+    
             $EXITCODE = 1
             if ($IS_READONLY -eq $false) {
-                # Added round brackets around get-content to fix file is in user issue.
-                #Ref: https://stackoverflow.com/a/10480811
-                $updatedContent = (Get-Content $hostFilePath);
-                $linenumber = $updatedContent | select-string "local.etherealengine.com";
-                $updatedContent[$linenumber.LineNumber - 1] = "$wslIp local.etherealengine.com api-local.etherealengine.com instanceserver-local.etherealengine.com 00000.instanceserver-local.etherealengine.com 00001.instanceserver-local.etherealengine.com 00002.instanceserver-local.etherealengine.com 00003.instanceserver-local.etherealengine.com";
-                Set-Content $hostFilePath $updatedContent;
+                Add-Content -Path $hostFilePath -value "`n$wslIp local.etherealengine.com api-local.etherealengine.com instanceserver-local.etherealengine.com 00000.instanceserver-local.etherealengine.com 00001.instanceserver-local.etherealengine.com 00002.instanceserver-local.etherealengine.com 00003.instanceserver-local.etherealengine.com";
+            }
+        }
+        
+        # Added this delay to ensure both hostfile entries gets updated, else it was skipping one of them.
+        Start-Sleep -Seconds 1;
+    
+        if ($hostFileContent -like "*microk8s.registry*") {
+            if ($hostFileContent -like "*$wslIp microk8s.registry*") {
+                Write-Host "microk8s.registry host entry exists";
+            }
+            else {
+                Write-Host "microk8s.registry host entry outdated";
+                
+                $EXITCODE = 1
+                if ($IS_READONLY -eq $false) {
+                    #Ref: https://shellgeek.com/read-file-line-by-line-in-powershell/
+                    $updatedContent = [System.IO.File]::ReadAllLines($hostFilePath);
+                    $linenumber = $updatedContent | select-string "microk8s.registry";
+                    $updatedContent[$linenumber.LineNumber - 1] = "$wslIp microk8s.registry";
+                    
+                    # Added this to avoid file in user error.
+                    $fileLocked = Test-FileLock -Path $hostFilePath
+                    while ($fileLocked -eq $true) {
+                        Write-Host "hostfile is locked. Waiting for 2 secs.";
+    
+                        Start-Sleep -Seconds 2;
+    
+                        $fileLocked = Test-FileLock -Path $hostFilePath
+                    }
+
+                    [System.IO.File]::WriteAllLines($hostFilePath, $updatedContent);
+                }
+            }
+        }
+        else {
+            Write-Host "microk8s.registry host entry needs to be added";
+    
+            $EXITCODE = 1
+            if ($IS_READONLY -eq $false) {
+                Add-Content -Path $hostFilePath -value "`n$wslIp microk8s.registry";
             }
         }
     }
     else {
-        Write-Host "*.etherealengine.com host entry needs to be added";
-
+        Write-Host "*.etherealengine.com & microk8s.registry host entries needs to be created";
+        
         $EXITCODE = 1
         if ($IS_READONLY -eq $false) {
-            Add-Content -Path $hostFilePath -value "`n$wslIp local.etherealengine.com api-local.etherealengine.com instanceserver-local.etherealengine.com 00000.instanceserver-local.etherealengine.com 00001.instanceserver-local.etherealengine.com 00002.instanceserver-local.etherealengine.com 00003.instanceserver-local.etherealengine.com";
+            Set-Content $hostFilePath "`n$wslIp local.etherealengine.com api-local.etherealengine.com instanceserver-local.etherealengine.com 00000.instanceserver-local.etherealengine.com 00001.instanceserver-local.etherealengine.com 00002.instanceserver-local.etherealengine.com 00003.instanceserver-local.etherealengine.com`n$wslIp microk8s.registry";
         }
     }
     
-    # Added this delay to ensure both hostfile entries gets updated, else it was skipping one of them.
-    Start-Sleep -Seconds 1;
+} catch {
+    Write-host "Failed in check-hostfile"
+    Write-Host $_
 
-    if ($hostFileContent -like "*microk8s.registry*") {
-        if ($hostFileContent -like "*$wslIp microk8s.registry*") {
-            Write-Host "microk8s.registry host entry exists";
-        }
-        else {
-            Write-Host "microk8s.registry host entry outdated";
-            
-            $EXITCODE = 1
-            if ($IS_READONLY -eq $false) {
-                # Added round brackets around get-content to fix file is in user issue.
-                #Ref: https://stackoverflow.com/a/10480811
-                $updatedContent = (Get-Content $hostFilePath);
-                $linenumber = $updatedContent | select-string "microk8s.registry";
-                $updatedContent[$linenumber.LineNumber - 1] = "$wslIp microk8s.registry";
-                Set-Content $hostFilePath $updatedContent;
-            }
-        }
-    }
-    else {
-        Write-Host "microk8s.registry host entry needs to be added";
-
-        $EXITCODE = 1
-        if ($IS_READONLY -eq $false) {
-            Add-Content -Path $hostFilePath -value "`n$wslIp microk8s.registry";
-        }
-    }
-}
-else {
-    Write-Host "*.etherealengine.com & microk8s.registry host entries needs to be created";
-    
-    $EXITCODE = 1
-    if ($IS_READONLY -eq $false) {
-        Set-Content $hostFilePath "`n$wslIp local.etherealengine.com api-local.etherealengine.com instanceserver-local.etherealengine.com 00000.instanceserver-local.etherealengine.com 00001.instanceserver-local.etherealengine.com 00002.instanceserver-local.etherealengine.com 00003.instanceserver-local.etherealengine.com`n$wslIp microk8s.registry";
-    }
+    Write-host "Exiting in 10 seconds"
+    Start-Sleep -Seconds 10;
+    $EXITCODE = 2;
 }
 
 exit $EXITCODE;
