afff11e7604201b03f56774b6c11f834d807307f
Hanzla Mateen <hanzlamateen@live.com>
2023-03-07 05:04:04 +0800

    Fixed issue with helm repo domain

--- assets/scripts/check-helm-repos.sh
+++ assets/scripts/check-helm-repos.sh
@@ -8,7 +8,7 @@ set -e
 
 helm repo add agones https://agones.dev/chart/stable
 helm repo add redis https://charts.bitnami.com/bitnami
-helm repo add etherealengine https://helm.etherealengine.io
+helm repo add etherealengine https://helm.etherealengine.org
 
 helm repo update
 echo "helm repos added and updated"
