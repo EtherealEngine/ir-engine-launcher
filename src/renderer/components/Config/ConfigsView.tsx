0238061b49b746e9dd5eacd223f8d70ffe4a296d
Hanzla Mateen <hanzlamateen@live.com>
2023-06-22 11:35:18 +0300

    Fixed issue with invalid wsl path (#105)
    * Fixed issue with invalid wsl path
    
    * Fixed formatting

--- src/renderer/components/Config/ConfigsView.tsx
+++ src/renderer/components/Config/ConfigsView.tsx
@@ -1,4 +1,5 @@
 import Channels from 'constants/Channels'
+import Endpoints from 'constants/Endpoints'
 import Storage from 'constants/Storage'
 import { OSType } from 'models/AppSysInfo'
 import { useSnackbar } from 'notistack'
@@ -40,9 +41,19 @@ const ConfigsView = ({ localConfigs, onChange, sx }: Props) => {
     if (path) {
       // On windows we need to make sure its WSL folder.
       if (appSysInfo.osType === OSType.Windows) {
-        const wslPrefixPath: string = await window.electronAPI.invoke(Channels.Utilities.GetWSLPrefixPath)
+        let wslPrefixPath: string = await window.electronAPI.invoke(Channels.Utilities.GetWSLPrefixPath)
+        // https://www.designcise.com/web/tutorial/how-to-remove-a-trailing-slash-from-a-string-in-javascript
+        wslPrefixPath = wslPrefixPath.endsWith('\\') ? wslPrefixPath.slice(0, -1) : wslPrefixPath
+
+        const wslDollarPrefixPath = wslPrefixPath.replace(
+          Endpoints.Paths.WSL_LOCALHOST_PREFIX,
+          Endpoints.Paths.WSL_$_PREFIX
+        )
+
         if (path.startsWith(wslPrefixPath)) {
           path = path.replace(wslPrefixPath, '').replaceAll('\\', '/')
+        } else if (path.startsWith(wslDollarPrefixPath)) {
+          path = path.replace(wslDollarPrefixPath, '').replaceAll('\\', '/')
         } else {
           enqueueSnackbar('Please select a folder in your WSL Ubuntu distribution.', { variant: 'error' })
           return
