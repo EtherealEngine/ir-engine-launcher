5706c64118472aff6a17ae0fefdb9cc0efbb5f4c
Moiz Adnan <67912355+MoizAdnan@users.noreply.github.com>
2023-07-17 11:00:56 +0500

    Implemented Engine tab (#112)
    * Added git pull functionality
    
    * Fixed git checkout functionality
    
    * Added safe directory command in scripts
    
    * Replaced replace() with trim()
    
    * Added engine tab in settings menu
    
    * Updated options
    
    * Updated remove engine deployment label
    
    * Added MySQL port command in force make admin
    
    * Added command to base cluster
    
    * Fixed formatting
    
    * Made UI adjustments for make admin button
    
    * Added check for engine tab and misc text changes
    
    ---------
    
    Co-authored-by: Hanzla Mateen <hanzlamateen@live.com>

--- src/main/Clusters/BaseCluster/BaseCluster.commands.ts
+++ src/main/Clusters/BaseCluster/BaseCluster.commands.ts
@@ -1,7 +1,8 @@
 const Commands = {
   IPFS_SECRET: "od  -vN 32 -An -tx1 /dev/urandom | tr -d ' \n'",
   DOCKER_STATS: "docker system df --format='{{json .}}';",
-  DOCKER_PRUNE: 'docker system prune -a -f;'
+  DOCKER_PRUNE: 'docker system prune -a -f;',
+  DEPLOYMENT_PRUNE: 'helm uninstall local;'
 }
 
 export default Commands
