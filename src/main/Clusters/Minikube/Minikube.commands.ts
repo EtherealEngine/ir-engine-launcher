fe03dc4a3944151dca960ef843da4f49369c3db7
Hanzla Mateen <hanzlamateen@live.com>
2023-07-18 15:22:52 +0500

    Added virtualbox prune support in minikube settings (#126)
    * Added virtualbox prune support in minikube settings
    
    * Added virtualbox dkms to uninstall as well

--- src/main/Clusters/Minikube/Minikube.commands.ts
+++ src/main/Clusters/Minikube/Minikube.commands.ts
@@ -2,7 +2,9 @@ const Commands = {
   DOCKER_STATS: "eval $(minikube docker-env); docker system df --format='{{json .}}'; eval $(minikube docker-env -u);",
   DOCKER_PRUNE: 'eval $(minikube docker-env); docker system prune -a -f; eval $(minikube docker-env -u);',
   DASHBOARD: 'minikube dashboard --url',
-  MINIKUBE_REMOVE: 'minikube delete --all --purge=true'
+  MINIKUBE_REMOVE: 'minikube delete --all --purge=true',
+  VIRTUALBOX_REMOVE: 'apt-get remove -y --purge virtualbox',
+  VIRTUALBOX_DKMS_REMOVE: 'apt-get remove -y --purge virtualbox-dkms '
 }
 
 export default Commands
