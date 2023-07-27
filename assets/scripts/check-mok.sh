e56361e1deffe20a38373999e9b81f1350dea793
MoizAdnan <moizadnan21@gmail.com>
2023-07-27 09:49:19 +0500

    Updated mok scripts

--- assets/scripts/check-mok.sh
+++ assets/scripts/check-mok.sh
@@ -13,12 +13,39 @@ SCRIPTS_FOLDER=$2
 # Functions
 #==========
 
-checkExitCode() {
-    exit_status=$?
-    if [ $exit_status -ne 0 ]; then
-        exit $exit_status
-    fi
+createMokFiles() {
+    username=$1
+    cd /var/lib/shim-signed/mok/
+    sudo openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.der -days 36500 -subj "/CN=My Name/"
+    openssl x509 -inform der -in MOK.der -out MOK.pem
+    exit 0
 }
+
+enrollMok() {
+
+    echo -e "Please first enter your account password. Then enter new password for MOK Manager.\n This password will be used after reboot, during the enrollment of MOK."
+
+    sudo mokutil --import /var/lib/shim-signed/mok/MOK.der
+
+    echo -e "Now the system needs to restart. After booting, you will be presented\n with the MOK Manager. In MOK Manager select 'Enroll MOK' and continue.\n
+    Enter the password you just entered when asked by the MOK Manager.\n After the process is complete, click 'Reboot'.\n
+    After booting run Ethereal Engine Control Center."
+    read -p "Do you want to restart the system? (y/n) " yn
+
+    case $yn in 
+        y ) echo Restarting...;;
+        n ) echo Exiting...;
+            exit 1;;
+        * ) echo invalid response;
+            exit 1;;
+    esac
+
+    echo "$password" | sudo -S systemctl reboot
+    exit 0
+}
+
+export -f enrollMok
+export -f createMokFiles
 
 #===========
 # Verify MOK
@@ -43,16 +70,12 @@ if echo "$PASSWORD" | sudo -S mokutil --sb-state | grep -q 'SecureBoot enabled';
         if [ -f /var/lib/shim-signed/mok/MOK.der ]; then
             echo "mok exists"
         else
-            echo "hello 4"
+            echo "creating mok files"
             echo "$PASSWORD" | sudo -S mkdir -p /var/lib/shim-signed/mok/
-            cd /var/lib/shim-signed/mok/
-            echo "$PASSWORD" | sudo -S openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.der -days 36500 -subj "/CN=My Name/"
-            echo "$PASSWORD" | sudo -S openssl x509 -inform der -in MOK.der -out MOK.pem
+            gnome-terminal --wait -- bash -c "createMokFiles $USERNAME;exec bash"
         fi
         echo "gnommee"
-        bash "$SCRIPTS_FOLDER/enroll-mok.sh" "$PASSWORD"
-
-        checkExitCode
+        gnome-terminal --wait -- bash -c "enrollMok;exec bash"
 
     fi
 fi        
